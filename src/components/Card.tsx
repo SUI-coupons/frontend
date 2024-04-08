fc1788e697567c6fc803c74563cabb2bf8f11e11
hoangquocvietuet <hoangquocvietuet@gmail.com>
2024-04-09 02:14:37 +0700

    updated dashboard

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,22 @@
 interface CardProps {
-    name: string
-    company: string
+    description: string
+    brand: string
     status: string
     price: string
+    discount: string
     image: string
 }
 
-export function Card({ name, company, status, price, image }: CardProps) {
+export function Card({
+    description,
+    brand,
+    status,
+    price,
+    discount,
+    image,
+}: CardProps) {
+    const dateFormat = new Date(status * 1000).toLocaleDateString()
+    const valid = new Date(status * 1000) > new Date()
     return (
         <div className='card border border-[#FFFFFF14] p-4 rounded-xl'>
             <div className='w-[275px] h-[250px] flex items-center bg-black rounded-xl'>
@@ -18,15 +28,19 @@ export function Card({ name, company, status, price, image }: CardProps) {
             <section className='mt-4 flex flex-col gap-4'>
                 <div className='flex justify-between items-center px-3'>
                     <div>
-                        <p className='text-sm text-[#FFFFFF99]'>by {company}</p>
-                        <h3 className='font-bold'>{name}</h3>
+                        <p className='text-sm text-[#FFFFFF99]'>by {brand}</p>
+                        <h3 className='font-bold'>{description}</h3>
                     </div>
-                    <p className='text-[#FFFFFF99]'>50% OFF</p>
+                    <p className='text-[#FFFFFF99]'>
+                        {(parseFloat(discount) * 100).toString()}% OFF
+                    </p>
                 </div>
                 <div className='grid grid-cols-2 bg-[#FFFFFF1A] w-full rounded-lg px-3 py-2'>
                     <div>
-                        <p className='text-[#FFFFFF99] text-[13px]'>Status</p>
-                        <p>{status}</p>
+                        <p className='text-[#FFFFFF99] text-[13px]'>
+                            Available status
+                        </p>
+                        <p>{dateFormat}</p>
                     </div>
                     {price ? (
                         <div>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,13 +1,68 @@
 import { Flex } from '@radix-ui/themes'
 import { Card } from '../components/Card'
+import { useEffect, useState } from 'react'
+import { useSuiClientQuery } from '@mysten/dapp-kit'
+
+function CouponData({ coupon_id }: { coupon_id: string }) {
+    const { data } = useSuiClientQuery('getObject', {
+        id: coupon_id,
+        options: {
+            showType: true,
+            showOwner: true,
+            showPreviousTransaction: true,
+            showDisplay: false,
+            showContent: true,
+            showBcs: false,
+            showStorageRebate: true,
+        },
+    })
+
+    if (data) {
+        const { id, brand, itemDiscount, discount, expirationDate, publisher } =
+            data.data.content.fields
+        return (
+            <Card
+                description={`${itemDiscount}`}
+                brand={`${brand}`}
+                status={expirationDate.toString()}
+                discount={(discount / 200).toString()}
+            />
+        )
+    }
+}
 
 export function Dashboard() {
+    const [listAvailableCoupons, setListAvailableCoupons] = useState([])
+
+    const { data } = useSuiClientQuery('getObject', {
+        id: import.meta.env.VITE_STATE_OBJECT_ID,
+        options: {
+            showType: true,
+            showOwner: true,
+            showPreviousTransaction: true,
+            showDisplay: false,
+            showContent: true,
+            showBcs: false,
+            showStorageRebate: true,
+        },
+    })
+
+    useEffect(() => {
+        if (data) {
+            const listCoupons = data?.data?.content?.fields?.availableCoupons
+            setListAvailableCoupons(listCoupons)
+        }
+    }, [data])
+
     return (
-        <div className='flex justify-between'>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-        </div>
+        listAvailableCoupons && (
+            <div className='flex justify-between'>
+                {listAvailableCoupons.map(
+                    (coupon_id: string, index: number) => (
+                        <CouponData coupon_id={coupon_id} key={index} />
+                    ),
+                )}
+            </div>
+        )
     )
 }
