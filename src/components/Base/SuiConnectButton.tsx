51aa3a72bf41939528515494923b84ff3df858de
hoangquocvietuet <hoangquocvietuet@gmail.com>
2024-04-10 02:34:39 +0700

    formatted

--- src/components/Base/SuiConnectButton.tsx
+++ src/components/Base/SuiConnectButton.tsx
@@ -1,8 +1,8 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { ConnectButton } from '@mysten/dapp-kit';
+import { ConnectButton } from '@mysten/dapp-kit'
 
 export function SuiConnectButton() {
-	return <ConnectButton />;
+    return <ConnectButton />
 }
